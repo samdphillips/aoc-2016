8324c6ffbb12d83dee231961c08e0dd95d1bb583
Sam Phillips <samdphillips@gmail.com>
2016-12-20 19:28:28 -0800

    fixed reading

--- src/bin/day-01.rs
+++ src/bin/day-01.rs
@@ -22,6 +22,18 @@ impl Dir {
 #[derive(Debug, PartialEq)]
 enum Turn { Left, Right }
 
+impl FromStr for Turn {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Turn, String> {
+        match s {
+            "L" => Ok(Turn::Left),
+            "R" => Ok(Turn::Right),
+            _ => Err("Unknown turn".to_string())
+        }
+    }
+}
+
 #[derive(Debug, PartialEq)]
 struct Command {
     turn: Turn,
@@ -51,25 +63,68 @@ impl Position {
 
         Position { x: self.x + dx, y: self.y + dy, facing: dir }
     }
-}
 
-fn read_turn(input: Chars) -> Option<Turn> {
-    let mut input = input.skip_while(|c| *c == ' ' || *c == ',');
-
-    match input.next() {
-        Some('L') => Some(Turn::Left),
-        Some('R') => Some(Turn::Right),
-        _ => None
+    fn distance_to_origin(&self) -> u32 {
+        (self.x.abs() + self.y.abs()) as u32
     }
 }
 
-fn read_steps(input: Chars) -> Option<u32> {
-    let s : String = input.take_while(|c| c.is_numeric()).collect();
-    u32::from_str(&s).ok()
+struct Tokenize<'a> {
+    inner: Chars<'a>
+}
+
+impl<'a> Tokenize<'a> {
+    fn from_str(s: &'a str) -> Tokenize<'a> {
+        Tokenize { inner: s.chars() }
+    }
+}
+
+impl<'a> Iterator for Tokenize<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        loop {
+            let mut opt_ch = self.inner.next();
+            if opt_ch.is_none() { return None }
+
+            let mut ch = opt_ch.unwrap();
+            if ch == ',' || ch == ' ' {
+                continue
+            }
+
+            if ch == 'L' || ch == 'R' {
+                return Some(ch.to_string())
+            }
+
+            if ch.is_digit(10) {
+                let mut s = String::new();
+                loop {
+                    s.push(ch);
+                    opt_ch = self.inner.next();
+                    if opt_ch.is_none() || !opt_ch.unwrap().is_digit(10) {
+                        return Some(s)
+                    }
+                    ch = opt_ch.unwrap();
+                }
+            }
+        }
+    }
+}
+
+fn read_command(tok: &mut Tokenize) -> Option<Command> {
+    let turn = tok.next();
+    if turn.is_none() { return None }
+
+    let steps = tok.next();
+    if steps.is_none() { return None }
+
+    let turn : Turn = turn.unwrap().parse().expect("expected a turn");
+    let steps : u32 = steps.unwrap().parse().expect("expected a u32");
+    Some(Command { turn: turn, steps: steps })
 }
 
 #[test]
-fn test_turns() {
+fn aoc01_test_turns() {
     assert!(Dir::N.turn(Turn::Left) == Dir::W);
     assert!(Dir::N.turn(Turn::Right) == Dir::E);
 
@@ -84,25 +139,33 @@ fn test_turns() {
 }
 
 #[test]
-fn test_update_position() {
+fn aoc01_test_update_position() {
     let cmd = Command { turn: Turn::Left, steps: 5 };
     let posn = Position { x: 0, y: 0, facing: Dir::N };
     assert!(posn.update(cmd) == Position { x: -5, y: 0, facing: Dir::W });
 }
 
 #[test]
-fn test_read_turn() {
-    assert!(read_turn("L".chars()) == Some(Turn::Left));
-    assert!(read_turn("R".chars()) == Some(Turn::Right));
-    assert!(read_turn("   R".chars()) == Some(Turn::Right));
-    assert!(read_turn(",  R".chars()) == Some(Turn::Right));
-    assert!(read_turn("".chars()) == None);
+fn aoc01_test_tokenize() {
+    let mut tok = Tokenize::from_str(",  L   ,   32");
+    assert!(tok.next() == Some("L".to_string()));
+    assert!(tok.next() == Some("32".to_string()));
+    assert!(tok.next() == None);
 }
 
 #[test]
-fn test_read_steps() {
-    assert!(read_steps("1234".chars()) == Some(1234));
-    assert!(read_steps("1234, L1233".chars()) == Some(1234));
+fn aoc01_test_read_command() {
+    let mut tok = Tokenize::from_str("L32, R2, L5");
+    assert!(read_command(&mut tok) == Some(Command { turn: Turn::Left, steps: 32 }));
+    assert!(read_command(&mut tok) == Some(Command { turn: Turn::Right, steps: 2 }));
+    assert!(read_command(&mut tok) == Some(Command { turn: Turn::Left, steps: 5 }));
+    assert!(read_command(&mut tok) == None)
+}
+
+#[test]
+fn aoc01_test_distance() {
+    let posn = Position { x: 2, y: -2, facing: Dir::N };
+    assert!(posn.distance_to_origin() == 4);
 }
 
 fn main () {
