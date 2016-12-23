f07b7c27ccf79a04d8b300b96e483bc282940fd2
Sam Phillips <samdphillips@gmail.com>
2016-12-23 00:27:08 -0800

    day 2 part 1 complete

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,5 +1,6 @@
 
 use std::cmp::{max, min};
+use std::io::BufRead;
 
 fn sub1(x: u8) -> u8 {
     max(0, (x as i8) - 1) as u8
@@ -20,7 +21,6 @@ impl State {
 
 fn decode(mut state: State, st: &str) -> (State, u8) {
     for c in st.chars() {
-        print!("{:?}", state);
         state = match c {
             'U' => State(state.0, sub1(state.1)),
             'D' => State(state.0, add1(state.1)),
@@ -28,21 +28,38 @@ fn decode(mut state: State, st: &str) -> (State, u8) {
             'R' => State(add1(state.0), state.1),
             _ => panic!("Unknown character")
         };
-        println!(" -[{}]-> {:?}", c, state);
     }
-
-    println!("{:?} {}", state, state.value());
     (state, state.value())
 }
 
-fn part_one() {
-    let state = State(1, 1);
-    let (state, v) = decode(state, "ULL");
-    let (state, v) = decode(state, "RRDDD");
-    let (state, v) = decode(state, "LURDL");
-    let (state, v) = decode(state, "UUUUD");
+fn decode_lines(input: &mut BufRead) -> Vec<u8> {
+    let mut state = State(1, 1);
+    let mut code = Vec::new();
+
+    for line in input.lines() {
+        let (s, v) = decode(state, &line.unwrap());
+        state = s;
+        code.push(v)
+    }
+    code
 }
 
+#[test]
+fn aoc02_test1() {
+    use std::io::Cursor;
+    let input = "ULL\nRRDDD\nLURDL\nUUUUD";
+    let mut input = Cursor::new(input);
+    let code = decode_lines(&mut input);
+    assert!(code == [1, 9, 8, 5]);
+}
+
+fn part_one() {
+    let stdin = std::io::stdin();
+    let mut input = stdin.lock();
+    let code = decode_lines(&mut input);
+
+    println!("{:?}", code);
+}
 
 fn part_two() { }
 
@@ -53,6 +70,5 @@ fn main() {
         "-1" => part_one(),
         "-2" => part_two(),
         _ => println!("expected '-1' or '-2'")
-
     }
 }
