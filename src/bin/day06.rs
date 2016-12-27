df24b5edc1a46bba48bfa43c97b819dce4630a0b
Sam Phillips <samdphillips@gmail.com>
2016-12-27 15:42:01 -0800

    day 6 part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -22,9 +22,15 @@ impl Counter {
         entries.sort_by(|&(_, a), &(_, b)| b.cmp(&a));
         entries[0].0
     }
+
+    fn bottom(&self) -> char {
+        let mut entries: Vec<(char, u32)> = self.counts.iter().map(|(ch, c)| (*ch, *c)).collect();
+        entries.sort_by(|&(_, a), &(_, b)| a.cmp(&b));
+        entries[0].0
+    }
 }
 
-fn main() {
+fn part_one() {
     let stdin = std::io::stdin();
     let mut cs: Vec<Counter> = (0..8).map(|_| Counter::new()).collect();
 
@@ -33,5 +39,27 @@ fn main() {
             cs[i].add_char(ch)
         }
         println!("{}", cs.iter().map(|c| c.top()).collect::<String>());
+    }
+}
+
+fn part_two() {
+    let stdin = std::io::stdin();
+    let mut cs: Vec<Counter> = (0..8).map(|_| Counter::new()).collect();
+
+    for line in stdin.lock().lines() {
+        for (i, ch) in line.unwrap().chars().enumerate() {
+            cs[i].add_char(ch)
+        }
+        println!("{}", cs.iter().map(|c| c.bottom()).collect::<String>());
+    }
+}
+
+fn main () {
+    let flag = std::env::args().nth(1).unwrap();
+
+    match flag.as_ref() {
+        "-1" => part_one(),
+        "-2" => part_two(),
+        _ => println!("expected '-1' or '-2'")
     }
 }
