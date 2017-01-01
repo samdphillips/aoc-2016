e520416cf0bde0b4e071923882947b74bc64728b
Sam Phillips <samdphillips@gmail.com>
2017-01-01 00:17:22 -0800

    Another stab at day 9
    Will probably not work on this anymore.  The idea was that you could
    have a fancy Decompress reader that would work with a kind of push back
    buffer.  Pretty sure there a billion edge cases ...

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -49,6 +49,56 @@ impl<I> Read for FilterWSReader<I>
 }
 
 
+struct ExtBufReader<R> {
+    inner: R,
+    buf: Box<[u8]>,
+    pos: usize,
+    cap: usize,
+}
+
+impl<R> ExtBufReader<R>
+    where R: Read
+{
+    fn new(inner: R) -> ExtBufReader<R> {
+        ExtBufReader {
+            inner: inner,
+            buf: vec![0; 16].into_boxed_slice(),
+            pos: 0,
+            cap: 0
+        }
+    }
+
+    fn resize_buffer(&mut self, size: usize) -> Result<()> {
+        let orig_size = self.cap - self.pos;
+        let size = size + orig_size;
+        let mut new_buf: Box<[u8]> = vec![0; size].into_boxed_slice();
+        try!(self.buf[self.pos..self.cap].as_ref().read(&mut new_buf[size-orig_size..size]));
+        self.buf = new_buf;
+        self.pos = size - orig_size;
+        self.cap = size;
+        Ok(())
+    }
+
+    fn prepend_buffer(&mut self, mut data: &[u8]) -> Result<()> {
+        let data_size = data.len();
+        if data_size > self.pos {
+            try!(self.resize_buffer(data_size))
+        }
+        try!(data.read(&mut self.buf[self.pos - data_size..self.pos]));
+        self.pos = self.pos - data_size;
+        Ok(())
+    }
+}
+
+#[test]
+fn foo() {
+    use std::io::Cursor;
+    let mut a = ExtBufReader::new(Cursor::new("abc"));
+    a.cap = a.inner.read(&mut a.buf).unwrap();
+    println!("{:?}", a.buf);
+    a.prepend_buffer(b"abcdef").unwrap();
+    println!("{:?}", a.buf);
+}
 
 fn main() { }
 
